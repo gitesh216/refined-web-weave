5d8ebd2f2aacaa9e2c9a23310dc78427dff57988
gitesh216 <109015479+gitesh216@users.noreply.github.com>
2025-07-06 18:16:00 +0530

    fix: correct google font import statement

--- vite.config.ts
+++ vite.config.ts
@@ -6,13 +6,13 @@ import { componentTagger } from "lovable-tagger";
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
-    host: "::",
+    host: "127.0.0.1",
     port: 8080,
   },
   plugins: [
     react(),
-    mode === 'development' &&
-    componentTagger(),
+    // mode === 'development' &&
+    // componentTagger(),
   ].filter(Boolean),
   resolve: {
     alias: {
